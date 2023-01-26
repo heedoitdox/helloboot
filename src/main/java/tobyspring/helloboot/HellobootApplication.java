5e41973ac671e10f70beaa2604abee25b0908932
heedoitdox <binzzang810@gmail.com>
2023-01-27 01:14:54 +0900

    feature: @ComponentScan, Bean 의 라이프 사이클 메서드

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,21 +3,24 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
 	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
+	public ServletWebServerFactory servletWEbServerFactory() {
+		return new TomcatServletWebServerFactory();
 	}
 
 	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
 	}
 
 	public static void main(String[] args) {
@@ -28,10 +31,13 @@ public class HellobootApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 
-				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				dispatcherServlet.setApplicationContext(this);
+
 				WebServer webServer = serverFactory.getWebServer(servletContext ->
 						servletContext.addServlet("dispatcherServlet",
-								new DispatcherServlet(this)
+								dispatcherServlet
 						).addMapping("/*")
 
 				);
